15040d28f61a8ff1b54364c4d308d9e275c2a4af
Lopez <nolween.lopez@gmail.com>
2021-07-14 16:24:01 +0200

    Installation Vuetify + Début Header To Do: Communication IMDB

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,79 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-app>
+    <v-app-bar app absolute color="blue darken-3" elevate-on-scroll tile>
+      <v-icon class="white--text text-h4 pointer">mdi-filmstrip</v-icon>
+      <v-toolbar-title class="white--text text-h4 font-weight-bold">Movies DB</v-toolbar-title>
+      <span class="text-h5 white--text ml-10">Découvrez les meilleurs films de tous les temps ici !</span>
+      <template v-slot:extension>
+        <v-icon class="pb-3 white--text text-h4 pointer">mdi-format-title</v-icon
+        ><v-autocomplete hide details dark dense background-color="grey darken-3" filled class="mt-3 mx-2" clearable label="Titre" color="white"></v-autocomplete>
+        <v-icon class="pb-3 white--text text-h4 pointer">mdi-database</v-icon
+        ><v-autocomplete hide details dark dense background-color="grey darken-3" filled class="mt-3 mx-2" clearable label="Genre" color="white"></v-autocomplete>
+        <v-checkbox dark color="white" v-model="onlyUnseenMovies" label="Uniquement les films non vus"></v-checkbox>
+        <v-range-slider
+            v-model="years"
+            :max="maxYear"
+            :min="minYear"
+            hide-details
+            color="white"
+            class="align-center mb-4 ml-5"
+          >
+            <template v-slot:prepend>
+              <v-text-field
+                :value="years[0]"
+                dark
+                class="mt-0 pt-0"
+                hide-details
+                single-line
+                type="number"
+                style="width: 60px"
+                @change="$set(years, 0, $event)"
+              ></v-text-field>
+            </template>
+            <template v-slot:append>
+              <v-text-field
+                :value="years[1]"
+                dark
+                class="mt-0 pt-0"
+                hide-details
+                single-line
+                type="number"
+                style="width: 60px"
+                @change="$set(years, 1, $event)"
+              ></v-text-field>
+            </template>
+          </v-range-slider>
+      </template>
+    </v-app-bar>
+
+    <v-main>
+      <router-view />
+    </v-main>
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
+  name: "App",
 
-<style>
+  data: () => ({
+    onlyUnseenMovies: false,
+    years: [1970, 2021],
+    minYear:1970,
+    maxYear: 2021,
+  }),
+};
+</script>
+<style lang="scss">
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: "Roboto", Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+
+.pointer {
+  cursor: pointer;
 }
 </style>
