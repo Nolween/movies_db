c7000194829a735e12a5f98bc3704403561b3911
Lopez <nolween.lopez@gmail.com>
2021-07-15 16:56:53 +0200

    Liste Films avec infos  OK To Do: Filters + Slider Pagination

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,22 @@
 <template>
   <v-app>
-    <v-app-bar app absolute color="blue darken-3" elevate-on-scroll tile>
+    <v-app-bar app absolute color="blue darken-3" elevate-on-scroll tile height="100">
       <v-icon class="white--text text-h4 pointer">mdi-filmstrip</v-icon>
       <v-toolbar-title class="white--text text-h4 font-weight-bold">Movies DB</v-toolbar-title>
       <span class="text-h5 white--text ml-10">Découvrez les meilleurs films de tous les temps ici !</span>
       <template v-slot:extension>
         <v-icon class="pb-3 white--text text-h4 pointer">mdi-format-title</v-icon
-        ><v-autocomplete hide details dark dense background-color="grey darken-3" filled class="mt-3 mx-2" clearable label="Titre" color="white"></v-autocomplete>
+        ><v-autocomplete hide details dark dense background-color="grey darken-3" filled class="mb-3 mx-2" clearable label="Titre" color="white"></v-autocomplete>
         <v-icon class="pb-3 white--text text-h4 pointer">mdi-database</v-icon
-        ><v-autocomplete hide details dark dense background-color="grey darken-3" filled class="mt-3 mx-2" clearable label="Genre" color="white"></v-autocomplete>
-        <v-checkbox dark color="white" v-model="onlyUnseenMovies" label="Uniquement les films non vus"></v-checkbox>
+        ><v-autocomplete hide details dark dense background-color="grey darken-3" filled class="mb-3 mx-2" clearable label="Genre" color="white"></v-autocomplete>
+        <v-checkbox dark color="white" class="mb-3" v-model="onlyUnseenMovies" label="Uniquement les films non vus"></v-checkbox>
         <v-range-slider
             v-model="years"
             :max="maxYear"
             :min="minYear"
             hide-details
             color="white"
-            class="align-center mb-4 ml-5"
+            class="align-center mb-9 ml-5"
           >
             <template v-slot:prepend>
               <v-text-field
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,8 +1,49 @@
 <template>
-  <v-container>
+  <v-container fluid>
+    <v-row justify="center">
+      <v-col cols="8" lg="10">
+        <v-stepper alt-labels>
+          <v-stepper-header>
+            <template v-for="(movie, index) in getOrderedMovies">
+              <v-stepper-step step="" :key="index">
+                {{ dayjs(movie.release_date).format("DD/MM/YYYY") }}
+              </v-stepper-step>
+
+              <v-divider v-if="index !== getOrderedMovies.length - 1" :key="index"></v-divider>
+            </template>
+          </v-stepper-header>
+        </v-stepper>
+      </v-col>
+    </v-row>
     <v-row class="text-center">
-      <v-col cols="12">
-        <span v-for="(movie, index) in getOrderedMovies" :key="index" class="ml-3">{{movie.title}}</span>
+      <v-col cols="2" md="1" align-self="center" justify="center">
+        <v-btn v-if="movieIndex > 0" @click="movieIndex--" large rounded color="grey darken-3" class="mb-3"><v-icon class="white--text">mdi-chevron-left</v-icon></v-btn>
+        <v-btn v-if="movieIndex > 0" @click="movieIndex--" large rounded color="grey darken-3"><v-icon class="white--text">mdi-chevron-double-left</v-icon></v-btn>
+      </v-col>
+      <v-col cols="8" md="10">
+        <v-row no-gutters>
+          <v-col v-for="(movie, index) in getOrderedMovies" :key="index" lg="2" md="4" cols="6" class="movie-col">
+            <div class="white text-left pl-2 text-center">
+              <v-img :src="'https://image.tmdb.org/t/p/w300' + movie.poster_path" height="350" width="300"></v-img>
+            </div>
+            <div class="blue text-left pl-2 movie-title">
+              <span class="text-h6 white--text">{{ movie.title }}</span>
+            </div>
+            <div class="grey darken-3 text-left pl-2 genres-title">
+              <span class="text-h6 white--text">{{ getMovieGenres(movie.genre_ids) }}</span>
+            </div>
+            <div class="red darken-3 text-left pl-2">
+              <span class="text-h6 white--text">{{ dayjs(movie.release_date).format("DD/MM/YYYY") }}</span>
+            </div>
+            <div class="grey darken-3 text-left pa-4 text-center">
+              <span class="text-h6 white--text"><v-btn color="grey darken-3"><v-icon color="white" class="text-h5">mdi-bookmark</v-icon></v-btn></span>
+            </div>
+          </v-col>
+        </v-row>
+      </v-col>
+      <v-col cols="2" md="1" align-self="center">
+        <v-btn @click="movieIndex++" large rounded color="grey darken-3" class="mb-3"><v-icon class="white--text">mdi-chevron-right</v-icon></v-btn>
+        <v-btn @click="movieIndex++" large rounded color="grey darken-3"><v-icon class="white--text">mdi-chevron-double-right</v-icon></v-btn>
       </v-col>
     </v-row>
   </v-container>
@@ -10,29 +51,70 @@
 
 <script>
 import axios from "axios";
+//? Nécessaire pour les dates
+require("dayjs/locale/fr");
+var dayjs = require("dayjs");
 
 export default {
   name: "Home",
 
-  components: {
-  },
+  components: {},
   data() {
     return {
+      dayjs: dayjs,
       results: [],
+      movieIndex: 0,
+      genres: [],
     };
   },
   computed: {
     // Classement des films selon la date de sortie, et filtrés selon les champs
     getOrderedMovies() {
-      let movies = this.results
-      if (movies.length > 0 ) {
-      // Classement des films par date de sortie ascendante
-      return movies.sort((a, b) => (a.release_date >= b.release_date) ? 1 : -1)
+      let movies = this.results;
+      if (movies.length > 0) {
+        // Classement des films par date de sortie ascendante
+        movies = movies.sort((a, b) => (a.release_date >= b.release_date ? 1 : -1));
+
+        var indexMovie = this.movieIndex;
+        return movies.filter(function(element, index) {
+          return index >= indexMovie && index <= indexMovie + 5;
+        });
       }
-      return movies
-    }
+      // On ne garde que de l'index défini à +6
+
+      return movies;
+    },
   },
   methods: {
+    getMovieGenres(genreIdsArray) {
+      let genres = "";
+      for (let genreIdIndex in genreIdsArray) {
+        // Quel est l'index du genre par rapport à son ID dans la liste des genres?
+        let genreIndex = this.genres
+          .map(function(e) {
+            return e.id;
+          })
+          .indexOf(genreIdsArray[genreIdIndex]);
+        // Si on a bien l'index
+        if (genreIndex != -1) {
+          genres += this.genres[genreIndex].name + ", ";
+        }
+      }
+      // On vire la dernière virgule si on a quelque chose
+      if (genres !== "") {
+        genres = genres.substring(0, genres.length - 2);
+      }
+      return genres;
+    },
+    //  Récupération des films via l'API TMDB
+    getGenres() {
+      this.genres = [];
+      axios.get("https://api.themoviedb.org/3/genre/movie/list?api_key=" + process.env.VUE_APP_MOVIEDB_KEY + "&language=fr-FR").then((response) => {
+        for (let resultIndex in response.data?.genres) {
+          this.genres.push(response.data.genres[resultIndex]);
+        }
+      });
+    },
     //  Récupération des films via l'API TMDB
     getBestMovies() {
       this.results = [];
@@ -45,11 +127,19 @@ export default {
           }
         });
       }
-      
     },
   },
   beforeMount() {
+    this.getGenres();
     this.getBestMovies();
   },
 };
 </script>
+<style scoped>
+.movie-title, .genres-title {
+  height: 120px;
+}
+.movie-col {
+  border: 1px solid white;
+}
+</style>
